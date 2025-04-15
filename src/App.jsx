bc8524f9e542e92bb6403d3a81d590bdef73e524
Raj Soren <7rajkumarsoren@gmail.com>
2025-04-15 18:46:27 +0530

    added navbar section

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <main className='overflow-x-hidden antialiased text-neutral-800'>
+      <Navbar />
+    </main>
+  )
+}
+
+export default App
